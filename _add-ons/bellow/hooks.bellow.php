7eff818be59ef9fdc36feab8b09103ecf34e5a73
Jason Varga <varga.jason@gmail.com>
2014-02-14 19:16:40 -0500

    Added member registration hook option, changed up config and added default.yaml

--- _add-ons/bellow/hooks.bellow.php
+++ _add-ons/bellow/hooks.bellow.php
@@ -2,7 +2,11 @@
 
 class Hooks_bellow extends Hooks 
 {
-	private $order_details = array();
+	private $hipchat_endpoint = 'https://api.hipchat.com/v1/rooms/message';
+	private $slack_endpoint = 'https://slack.com/api/chat.postMessage';
+
+	private $order_details;
+	private $member;
 
 	/**
 	 * Run when checkout is completed
@@ -16,19 +20,18 @@ class Hooks_bellow extends Hooks
 			return;
 		}
 		$this->order_details = $order_details;
-		$this->notifyHipChat();
-		$this->notifySlack();
+		$this->notifyHipChatCheckoutComplete();
+		$this->notifySlackCheckoutComplete();
 	}
 
-	private function notifyHipChat()
+	private function notifyHipChatCheckoutComplete()
 	{
-		$config = $this->config['hipchat'];
+		$config = $this->config['hipchat']['checkout_complete'];
 		if (!$config['enabled'])
 			return;
 
-		$endpoint = 'https://api.hipchat.com/v1/rooms/message';
 		$query = http_build_query(array(
-			'auth_token' => $config['api_key'],
+			'auth_token' => $this->config['hipchat']['api_key'],
 			'from'       => $config['from'],
 			'room_id'    => $config['room_id'],
 			'color'      => $config['color'],
@@ -36,33 +39,98 @@ class Hooks_bellow extends Hooks
 			'message'    => strip_tags(Content::parse($config['message'], $this->order_details))
 		));
 
-		$url = $endpoint.'?'.$query;
+		$url = $this->hipchat_endpoint.'?'.$query;
 		$response = json_decode($this->performRequest($url));
 
 		if (isset($response->error)) {
-			$this->log->error('Error posting to HipChat: '.$response->error->message);
+			$this->log->error('Error posting to HipChat on checkout completion: '.$response->error->message);
 		}
 	}
 
-	private function notifySlack()
+	private function notifySlackCheckoutComplete()
 	{
-		$config = $this->config['slack'];
+		$config = $this->config['slack']['checkout_complete'];
 		if (!$config['enabled'])
 			return;
 
-		$endpoint = 'https://slack.com/api/chat.postMessage';
 		$query = http_build_query(array(
-			'token'    => $config['api_key'],
+			'token'    => $this->config['slack']['api_key'],
 			'channel'  => '#'.$config['channel'],
 			'username' => array_get($config, 'from', Config::getSiteName()),
 			'text'     => strip_tags(Content::parse($config['message'], $this->order_details))
 		));
 
-		$url = $endpoint.'?'.$query;
+		$url = $this->slack_endpoint.'?'.$query;
 		$response = json_decode($this->performRequest($url));
 		
 		if (!$response->ok) {
-			$this->log->error('Error posting to Slack: '.$response->error);
+			$this->log->error('Error posting to Slack on checkout completion: '.$response->error);
+		}
+	}
+
+	/**
+	 * Run when a member is registered
+	 *
+	 * @return string
+	 **/
+	function member__registration_complete($member)
+	{
+		if (!function_exists('curl_init')) {
+			$this->log->error('cURL is not installed.');
+			return;
+		}
+		$this->member = $member;
+		$this->notifySlackMemberRegistration();
+		$this->notifyHipChatMemberRegistration();
+	}
+
+	private function notifySlackMemberRegistration()
+	{
+		$config = $this->config['slack']['member_registration'];
+		if (!$config['enabled'])
+			return;
+
+		$username = $this->member->get('username');
+		$profile_data = Member::getProfile($username);
+
+		$query = http_build_query(array(
+			'token'    => $this->config['slack']['api_key'],
+			'channel'  => '#'.$config['channel'],
+			'username' => array_get($config, 'from', Config::getSiteName()),
+			'text'     => strip_tags(Content::parse($config['message'], $profile_data))
+		));
+
+		$url = $this->slack_endpoint.'?'.$query;
+		$response = json_decode($this->performRequest($url));
+		
+		if (!$response->ok) {
+			$this->log->error('Error posting to Slack on member registration: '.$response->error);
+		}
+	}
+
+	private function notifyHipChatMemberRegistration()
+	{
+		$config = $this->config['hipchat']['member_registration'];
+		if (!$config['enabled'])
+			return;
+
+		$username = $this->member->get('username');
+		$profile_data = Member::getProfile($username);
+
+		$query = http_build_query(array(
+			'auth_token' => $this->config['hipchat']['api_key'],
+			'from'       => $config['from'],
+			'room_id'    => $config['room_id'],
+			'color'      => $config['color'],
+			'notify'     => $config['notify'],
+			'message'    => strip_tags(Content::parse($config['message'], $profile_data))
+		));
+
+		$url = $this->hipchat_endpoint.'?'.$query;
+		$response = json_decode($this->performRequest($url));
+		
+		if (isset($response->error)) {
+			$this->log->error('Error posting to HipChat on member registration: '.$response->error->message);
 		}
 	}
 
