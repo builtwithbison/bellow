993fe8020b7c3e0b0c0e55c0e9f977af7aaa080f
Jason Varga <varga.jason@gmail.com>
2014-02-14 22:13:19 -0500

    Added Asana support and refactored some profile data setting

--- README.md
+++ README.md
@@ -1,9 +1,11 @@
 # Bellow
 ## An add-on for [Bison](https://builtwithbison.com) and [Statamic](http://statamic.com)
 
-> Call out to third parties when a customer performs a checkout.
+> Call out to third parties when a customer checks out or becomes a member.
 
-At the moment, Bellow can send messages to [HipChat](https://www.hipchat.com/) and [Slack](https://slack.com/).
+Bellow can:
+* Notify you in your [HipChat](https://www.hipchat.com/) or [Slack](https://slack.com/) rooms
+* Create tasks in [Asana](https://asana.com/)
 
 Let us know if you have any other suggestions for things you might want to happen at checkout.
 
@@ -13,6 +15,9 @@ Let us know if you have any other suggestions for things you might want to happe
 * Copy `_add-ons/bellow` to `_add-ons/bellow`
 * Copy `_config/add-ons/bellow` to `_config/add-ons/bellow`
 * Configure your service(s) in `bellow.yaml`.
+
+### Asana
+The easiest way to get your Asana workplace and user IDs is to go to https://app.asana.com/api/1.0/users/me once you've logged in to the app.
 
 ## Support
 
--- _add-ons/bellow/hooks.bellow.php
+++ _add-ons/bellow/hooks.bellow.php
@@ -4,9 +4,10 @@ class Hooks_bellow extends Hooks
 {
 	private $hipchat_endpoint = 'https://api.hipchat.com/v1/rooms/message';
 	private $slack_endpoint = 'https://slack.com/api/chat.postMessage';
+	private $asana_endpoint = 'https://app.asana.com/api/1.0/tasks';
 
 	private $order_details;
-	private $member;
+	private $profile_data;
 
 	/**
 	 * Run when checkout is completed
@@ -22,6 +23,7 @@ class Hooks_bellow extends Hooks
 		$this->order_details = $order_details;
 		$this->notifyHipChatCheckoutComplete();
 		$this->notifySlackCheckoutComplete();
+		$this->createAsanaTaskCheckoutComplete();
 	}
 
 	private function notifyHipChatCheckoutComplete()
@@ -68,6 +70,28 @@ class Hooks_bellow extends Hooks
 		}
 	}
 
+	private function createAsanaTaskCheckoutComplete()
+	{
+		$config = $this->config['asana']['checkout_complete'];
+		if (!$config['enabled'])
+			return;
+
+		$data = json_encode(array(
+			'data' => array(
+				'workspace' => $config['workspace'],
+				'assignee'  => $config['assignee'],
+				'name'      => strip_tags(Content::parse($config['task_name'], $this->order_details)),
+				'notes'     => strip_tags(Content::parse($config['notes'], $this->order_details))
+			)
+		));
+
+		$response = json_decode($this->performRequest($this->asana_endpoint, $data, $this->config['asana']['api_key']));
+		
+		if (isset($response->errors)) {
+			$this->log->error('Error posting to Asana on checkout completion: '.$response->errors[0]->message);
+		}
+	}
+
 	/**
 	 * Run when a member is registered
 	 *
@@ -79,9 +103,11 @@ class Hooks_bellow extends Hooks
 			$this->log->error('cURL is not installed.');
 			return;
 		}
-		$this->member = $member;
+		$username = $member->get('username');
+		$this->profile_data = Member::getProfile($username);
 		$this->notifySlackMemberRegistration();
 		$this->notifyHipChatMemberRegistration();
+		$this->createAsanaTaskMemberRegistration();
 	}
 
 	private function notifySlackMemberRegistration()
@@ -90,14 +116,11 @@ class Hooks_bellow extends Hooks
 		if (!$config['enabled'])
 			return;
 
-		$username = $this->member->get('username');
-		$profile_data = Member::getProfile($username);
-
 		$query = http_build_query(array(
 			'token'    => $this->config['slack']['api_key'],
 			'channel'  => '#'.$config['channel'],
 			'username' => array_get($config, 'from', Config::getSiteName()),
-			'text'     => strip_tags(Content::parse($config['message'], $profile_data))
+			'text'     => strip_tags(Content::parse($config['message'], $this->profile_data))
 		));
 
 		$url = $this->slack_endpoint.'?'.$query;
@@ -114,16 +137,13 @@ class Hooks_bellow extends Hooks
 		if (!$config['enabled'])
 			return;
 
-		$username = $this->member->get('username');
-		$profile_data = Member::getProfile($username);
-
 		$query = http_build_query(array(
 			'auth_token' => $this->config['hipchat']['api_key'],
 			'from'       => $config['from'],
 			'room_id'    => $config['room_id'],
 			'color'      => $config['color'],
 			'notify'     => $config['notify'],
-			'message'    => strip_tags(Content::parse($config['message'], $profile_data))
+			'message'    => strip_tags(Content::parse($config['message'], $this->profile_data))
 		));
 
 		$url = $this->hipchat_endpoint.'?'.$query;
@@ -134,11 +154,38 @@ class Hooks_bellow extends Hooks
 		}
 	}
 
-	private function performRequest($url)
+	private function createAsanaTaskMemberRegistration()
+	{
+		$config = $this->config['asana']['member_registration'];
+		if (!$config['enabled'])
+			return;
+
+		$data = json_encode(array(
+			'data' => array(
+				'workspace' => $config['workspace'],
+				'assignee'  => $config['assignee'],
+				'name'      => strip_tags(Content::parse($config['task_name'], $this->profile_data)),
+				'notes'     => strip_tags(Content::parse($config['notes'], $this->profile_data))
+			)
+		));
+
+		$response = json_decode($this->performRequest($this->asana_endpoint, $data, $this->config['asana']['api_key']));
+		
+		if (isset($response->errors)) {
+			$this->log->error('Error posting to Asana on member registration: '.$response->errors[0]->message);
+		}
+	}
+
+	private function performRequest($url, $data = null, $auth = null)
 	{
 		$ch = curl_init();
 		curl_setopt($ch, CURLOPT_URL, $url);
 		curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+		curl_setopt($ch, CURLOPT_HTTPHEADER, array('Content-Type: application/json'));
+		if ($auth)
+			curl_setopt($ch, CURLOPT_USERPWD, $auth);
+		if ($data)
+			curl_setopt($ch, CURLOPT_POSTFIELDS, $data);
 		$output = curl_exec($ch);
 		curl_close($ch);
 		return $output;
